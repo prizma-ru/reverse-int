eced1b7e0a854874e80aa5de47e5ab91edc17531
prizma-ru <dmitry.glotov@gmail.com>
2021-08-02 01:48:52 +0300

    fix: err gor NaN

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-function reverse (n) {
-    let realDigits = n.toString().split('').map(Number); //делаем из числа массив чисел разделаем на символы потом преобразуем строки к числам
+module.exports = function reverse(n) {
+    let realDigits = Math.abs(n).toString().split('').map(Number); //делаем из числа массив чисел разделаем на символы потом преобразуем строки к числам
     /* realDigits.sort(function (a, b) { return b - a; });  */
-    let newN =[];
-    for (let i = 0; i < realDigits.length; ++i){
+    let newN = [];
+    for (let i = 0; i < realDigits.length; ++i) {
         newN.unshift(realDigits[i]); //Метод unshift() добавляет один или более элементов в начало массива и возвращает новую длину массива.
     }
     return Number(newN.join('')); // склеиваем массив и преобразуем строку в число  
