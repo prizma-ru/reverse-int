c37169de6e30f435f071cf4b7ad185bdbecdc5c8
prizma-ru <dmitry.glotov@gmail.com>
2021-08-02 01:39:24 +0300

    init: start task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+    let realDigits = n.toString().split('').map(Number); //делаем из числа массив чисел разделаем на символы потом преобразуем строки к числам
+    /* realDigits.sort(function (a, b) { return b - a; });  */
+    let newN =[];
+    for (let i = 0; i < realDigits.length; ++i){
+        newN.unshift(realDigits[i]); //Метод unshift() добавляет один или более элементов в начало массива и возвращает новую длину массива.
+    }
+    return Number(newN.join('')); // склеиваем массив и преобразуем строку в число  
 }
